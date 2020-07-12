c013f6e5f0366ea29a080c71d1ae2375f95143d3
Marko Koskinen <markokoskinen2037@gmail.com>
2020-07-12 16:28:20 +0300

    git fix

---
+++ patientor-frontend/src/SinglePatientPage/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import axios from "axios";
+import { Header, Icon, SemanticICONS } from "semantic-ui-react";
+
+import { Patient, Gender } from "../types";
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+import { useParams } from "react-router-dom";
+
+const SinglePatientPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const [{ patients }, dispatch] = useStateValue();
+  React.useEffect(() => {
+
+    const fetchSinglePatient = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: "UPDATE_PATIENT_LIST", payload: patientFromApi });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchSinglePatient();
+  }, [dispatch]);
+
+  const patient = patients[id] 
+
+  if(!patient) return null
+
+  const getIconName = (): SemanticICONS => {
+    switch (patient.gender) {
+      case Gender.Female:
+        return "female"
+      case Gender.Male:
+        return "male"
+      case Gender.Other:
+        return "question"
+      default:
+        return "question"
+    }
+  }
+
+  const iconName = getIconName()
+
+  return (
+    <div className="singlePatient">
+      <Header as="h2">{patient.name} <Icon name={iconName}/> </Header>
+      <div>SSN: {patient.ssn}</div>
+      <div>occupation: {patient.occupation}</div>
+    </div>
+  );
+};
+
+export default SinglePatientPage;
