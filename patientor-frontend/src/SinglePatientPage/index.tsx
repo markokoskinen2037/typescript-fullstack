755975f6359be9bca72f6857419e31ffcebab23e
Marko Koskinen <markokoskinen2037@gmail.com>
2020-07-12 18:52:22 +0300

    9.22

--- patientor-frontend/src/SinglePatientPage/index.tsx
+++ patientor-frontend/src/SinglePatientPage/index.tsx
@@ -1,11 +1,89 @@
 import React from "react";
 import axios from "axios";
-import { Header, Icon, SemanticICONS } from "semantic-ui-react";
+import { Header, Icon, SemanticICONS, Segment } from "semantic-ui-react";
 
-import { Patient, Gender } from "../types";
+import { Patient, Gender, Entry, assertNever, Diagnosis } from "../types";
 import { apiBaseUrl } from "../constants";
 import { useStateValue, updatePatientListAction } from "../state";
 import { useParams } from "react-router-dom";
+
+const EntryDetails: React.FC<{entry:Entry, diagnoses:Diagnosis[]}> = ({entry, diagnoses}) => {
+
+    
+  const getDiagnosisName = (code:string) => {
+    const temp = diagnoses.find(d => {
+      return d.code === code
+    })
+
+    return temp?.name
+  }
+
+const getHpColor = (val:number) => {
+  switch (val) {
+    case 0:
+      return "green"
+    case 1:
+      return "yellow"
+    case 2:
+      return "red"
+    case 3:
+      return "black"
+        
+    default:
+      break;
+  }
+}
+ 
+
+  switch (entry.type) {
+    case "HealthCheck":
+
+
+    console.log(entry.healthCheckRating)
+      
+      return (
+        <Segment key={entry.id}>
+          <h4>{entry.date} <Icon size="large" name="doctor" /> </h4>
+          <span>{entry.description}</span>
+    
+          {entry.diagnosisCodes?.map((code) => (
+            <li key={code}>{code} - {getDiagnosisName(code)}</li>
+          ))}
+          <br/>
+          <br/>
+          <Icon name="heart" color={getHpColor(entry.healthCheckRating)} />
+        </Segment>
+      )
+    case "Hospital":
+      return (
+        <Segment key={entry.id}>
+          <h4>{entry.date} <Icon size="large" name="doctor" /> </h4>
+          <span>{entry.description}</span>
+          {entry.diagnosisCodes?.map((code) => (
+            <li key={code}>{code} - {getDiagnosisName(code)}</li>
+          ))}
+        </Segment>
+      )
+    case "OccupationalHealthcare":
+      return (
+        <Segment key={entry.id}>
+          <h4>{entry.date} <Icon size="large" name="warehouse" /> {entry.employerName} </h4>
+          <span>{entry.description}</span>
+          {entry.diagnosisCodes?.map((code) => (
+            <li key={code}>{code} - {getDiagnosisName(code)}</li>
+          ))}
+          {!!entry.sickLeave && <div style={{fontWeight:"bold"}}>
+            Sick leave from {entry.sickLeave.startDate} till {entry.sickLeave.endDate}
+            </div>}
+        </Segment>
+      )
+    default:
+      return assertNever(entry)
+  }
+
+
+}
+
 
 const SinglePatientPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -44,13 +122,7 @@ const SinglePatientPage: React.FC = () => {
 
   const iconName = getIconName()
 
-  const getDiagnosisName = (code:string) => {
-    const temp = diagnoses.find(d => {
-      return d.code === code
-    })
 
-    return temp?.name
-  }
 
   return (
     <div className="singlePatient">
@@ -58,18 +130,7 @@ const SinglePatientPage: React.FC = () => {
       <div>SSN: {patient.ssn}</div>
       <div>occupation: {patient.occupation}</div>
       {!!patient.entries.length && <h2>entries</h2>}
-      {patient.entries.map(entry => {
-        return (
-          <div key={entry.id}>
-          <div>{entry.date} {entry.description}</div>
-          <ul>
-          {entry.diagnosisCodes?.map((code) => (
-            <li key={code}>{code} - {getDiagnosisName(code)}</li>
-          ))}
-          </ul>
-        </div>
-      )
-      })}
+      {patient.entries.map(e => <EntryDetails key={id+e.id} entry={e} diagnoses={diagnoses} />)}
     </div>
   );
 };
--- patientor-frontend/src/types.ts
+++ patientor-frontend/src/types.ts
@@ -1,3 +1,9 @@
+export const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
 export interface Diagnosis {
   code: string;
   name: string;
